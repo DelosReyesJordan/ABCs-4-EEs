eb7854f5e2e015ae1a0755490855e3531ae10af8
DelosReyesJordan <63662211+DelosReyesJordan@users.noreply.github.com>
2025-06-23 02:16:49 -0700

    Update navbar.js

--- navbar.js
+++ navbar.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
-  fetch("navbar.html")
+  fetch("../navbar.html")
     .then(response => {
-      if (!response.ok) throw new Error("Failed to load navbar.html");
+      if (!response.ok) throw new Error("Navbar load failed");
       return response.text();
     })
     .then(data => {
